ddb058afebf366fb1f1d5aa5fc47103f1b3d2a4e
Nixon L <nicklazaro14@gmail.com>
2024-02-15 16:11:02 -0500

    hmm

--- python_backend/main.py
+++ python_backend/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Data" : "Testing"}
+
+@app.get("/about")
+def about():
+    return {"About" : "help"}
