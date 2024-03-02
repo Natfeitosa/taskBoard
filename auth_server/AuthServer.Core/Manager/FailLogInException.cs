651f4c5fb9018b2df0577bb670b58830e0262030
Natanael <feitosanat@gmail.com>
2024-03-02 11:46:26 -0500

    removed important keys

--- auth_server/AuthServer.Core/Manager/FailLogInException.cs
+++ auth_server/AuthServer.Core/Manager/FailLogInException.cs
@@ -3,7 +3,6 @@ namespace AuthServer.Core.Manager
 {
   public class FailLogInException : Exception
   {
-
     public FailLogInException() { }
     public FailLogInException(string message) : base(message) { }
     public FailLogInException(string message, Exception inner) : base(message, inner) { }
