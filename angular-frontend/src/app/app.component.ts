34fe0ba3c519cf363b07094999fd41a2b1ecc1b2
Nixon L <nicklazaro14@gmail.com>
2024-03-21 17:07:54 -0400

    idk what im doing tbh lmao

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, HomeComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'taskBoard';
+  constructor(private http: HttpClient){}
+
+  public fetchDetails() {
+
+  }
 }
